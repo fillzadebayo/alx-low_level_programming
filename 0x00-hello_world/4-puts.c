f731c141a9fbaa810e712864638b37df79366eb1
fillzadebayo <you@example.com>
2022-09-09 14:54:51 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main - entry point
+ *
+ * Return: Always 0 (sucsess)
+ *
+ */
+
+int main (void)
+{
+	char MyStr[]= "Programming is like building a multilingual puzzle.\n";
+	puts(MyStr);
+	return 0;
+}
