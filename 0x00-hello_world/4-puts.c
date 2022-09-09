b11fba0b310b7725d1283d6814704f82756ec808
fillzadebayo <you@example.com>
2022-09-09 15:04:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,9 @@
 
 int main (void)
 {
-	char MyStr[]= "Programming is like building a multilingual puzzle.\n";
-	puts(MyStr);
-	return 0;
+	
+
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
 }
