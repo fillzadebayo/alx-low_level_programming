95dcfb37d6ffbcb71668148fbbedb3e5132ed648
fillzadebayo <you@example.com>
2022-09-14 07:11:46 -0700

    fibonanci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int i, n, n1, n2, nextTerm;
+	int i, n, n1, n2;
 		n = 50;
 		n1 = 0;
 		n2 = 1;
-	nextTerm = 1;
 	for (i = 1; i <= n; ++i)
 	{
-		printf("%d, ", nextTerm);
-		n1 = n2;
-		n2 = nextTerm;
+		printf("%d, ", n2);
 		nextTerm = n1 + n2;
+		int next = n1 + n2
+			n1 = n2;
+		n2 = next;
 	}
 return (0);
 }
