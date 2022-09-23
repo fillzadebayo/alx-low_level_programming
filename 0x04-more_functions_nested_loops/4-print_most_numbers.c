1fd7f9275b5cd3b0a366e9ab28870592d0412a03
fillzadebayo <you@example.com>
2022-09-23 14:32:16 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print all save 2 & 4
+ *
+ * Return: returns void
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n == 50 || n == 52)
+		{
+		}
+		else
+			putchar(n);
+	}
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1 - isdigit
 2 - mul
 3 - print num
+4 - print most
